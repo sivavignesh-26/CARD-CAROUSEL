735fbfcd50726a005d8c13bef5584d5787adc7f7
sivavignesh-26 <sivavigneshg26@gmail.com>
2024-12-24 21:25:29 +0530

    Add files via upload

---
+++ assets/js/app.js
@@ -0,0 +1,37 @@
+new Swiper('.card-wrapper', {
+    loop: true,
+    spaceBetween: 30,
+  
+    //pagination bullets
+    pagination: {
+      el: '.swiper-pagination',
+      clickable : true,
+      dynamicBullets : true
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+
+    //Responsive Breakpoints
+    breakpoints: {
+        0: {
+            slidesPerView: 1
+        },
+
+        768: {
+            slidesPerView: 2
+        },
+
+        1024: {
+            slidesPerView: 3
+        },
+    }
+  });
